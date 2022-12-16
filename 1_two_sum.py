3900f0c6ee7db04d86e9e056b0a844daefc8b670
edsonibarra <edsonibarra97@gmail.com>
2022-12-16 17:55:56 -0600

    initial commit

---
+++ 1_two_sum.py
@@ -0,0 +1,11 @@
+"""
+Link to challenge: https://leetcode.com/problems/two-sum
+"""
+from typing import List
+
+
+def two_sum(nums: List[int], target: int) -> List[int]:
+    """
+    """
+    pass
+
