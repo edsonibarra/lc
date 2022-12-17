eadef107f237604ae9b8fa2ef839ede6b45e21d4
edsonibarra <edsonibarra97@gmail.com>
2022-12-16 19:17:22 -0600

    add type hints and better documentation

--- src/challenges/two_sum.py
+++ src/challenges/two_sum.py
@@ -1,16 +1,18 @@
 """
 Link to challenge: https://leetcode.com/problems/two-sum
 """
-from typing import List
+from typing import List, Dict
 
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     """
+
     Complexity:
         Time O(n)
         Space O(n)
+
     """
-    seen = dict()
+    seen: Dict[int, int] = dict()
     for i, n in enumerate(nums):
         result: int = target - n
         if result in seen:
@@ -21,20 +23,21 @@ def two_sum_2(nums: List[int], target: int) -> List[int]:
     """
 
     Note: This will only work when you are asked for the numbers intead of
-    the indices.
+    the indices because nums is modified
 
     Complexity:
         Time O(nLog(n))
         Space O(1)
+
     """
     nums.sort()
-    left = 0
-    right = len(nums) - 1
+    left: int = 0
+    right: int = len(nums) - 1
 
     while left < right:
-        current_sum = nums[left] + nums[right]
+        current_sum: int = nums[left] + nums[right]
         if current_sum == target:
-            return [left, right]
+            return [nums[left], nums[right]]
         elif current_sum < target:
             left += 1
         else:
